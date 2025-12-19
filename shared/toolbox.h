e6f2754c0ca31cfbc5e03a62cf030afb23e26800
Auberon Lopez <auberon.lopez@greenriver.edu>
2025-12-19 13:28:37 -0800

    Moved toolbox to shared location

--- shared/toolbox.h
+++ shared/toolbox.h
@@ -8,7 +8,7 @@ typedef unsigned short u16;
 typedef unsigned int u32;
 
 // volatile types - used to indeicate to compiler that seemingly useless writes shouldn't be optimized away
-// commonly used for registers / mmap 
+// commonly used for registers / mmap :o
 typedef volatile u8 vu8;
 typedef volatile u16 vu16;
 typedef volatile u32 vu32;
